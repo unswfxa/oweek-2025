44670e4c693de5f0b2afc99286832df3cd190877
agnes-tjokrosetio <agnes.tjokrosetio@gmail.com>
2025-12-19 23:25:55 +1100

    initial landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+    <div className="w-screen flex flex-col">
+      <div className="flex flex-col justify-center items-center gap-12 m-16">
+        <h1 className="text-center">Trading Stocks</h1>
+        <p className="text-xl text-center">
+          You’ll be given a series of real-world news events from around the globe. You must decide whether to buy,
+          sell, or hold. Every decision affects your balance — some will pay off, others won’t. After all prompts, your
+          final balance determines your prize.
         </p>
+        <div className="text-xl flex flex-col justify-center items-center gap-6">
+          <p className="text-2xl text-center">
+            Starting Balance: <b>20,000</b>
+          </p>
+          <button
+            style={{
+              background: "linear-gradient(135deg, #56CDD3 0%, #3596E3 100%)",
+              color: "white",
+            }}
+            onClick={() => console.log("start trading")}
+          >
+            <b>Start Trading</b>
+          </button>
+        </div>
+        <p className="text-xl text-center">Think fast, trade smart, and maximise your gains 💸</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
