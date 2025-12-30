b0f98a1b6719340fac5d4c883b309cca44b0195f
Anthony Law <tacenda2143@gmail.com>
2025-12-31 02:27:05 +1100

    linked components to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
 import { useState } from "react";
+import TradeActions from "./TradeActions";
+import TradePrompt from "./TradePrompt";
+import Portfolio from "./Portfolio";
 
 function App() {
-  return (
-    <div className="w-screen flex flex-col">
-      <div className="flex flex-col justify-center items-center gap-12 m-16">
-        <h1 className="text-center">Trading Stocks</h1>
-        <p className="text-xl text-center">
-          You’ll be given a series of real-world news events from around the globe. You must decide whether to buy,
-          sell, or hold. Every decision affects your balance — some will pay off, others won’t. After all prompts, your
-          final balance determines your prize.
-        </p>
-        <div className="text-xl flex flex-col justify-center items-center gap-6">
-          <p className="text-2xl text-center">
-            Starting Balance: <b>20,000</b>
-          </p>
-          <button
-            style={{
-              background: "linear-gradient(135deg, #56CDD3 0%, #3596E3 100%)",
-              color: "white",
-            }}
-            onClick={() => console.log("start trading")}
-          >
-            <b>Start Trading</b>
-          </button>
-        </div>
-        <p className="text-xl text-center">Think fast, trade smart, and maximise your gains 💸</p>
+  const [cash, setCash] = useState(20000);
+  const [prompt, setPrompt] = useState("AUD/USD rises after strong jobs data");
+
+  const handleBuy = () => {
+    console.log("Buy clicked");
+    setCash((prev) => prev + 500);
+  };
+
+  const handleSell = () => {
+    console.log("SELL clicked");
+    setCash((prev) => prev - 500);
+  };
+
+  const handleHold = () => {
+    console.log("HOLD clicked");
+  }
+
+return (
+  <div className="w-screen flex flex-col">
+    <div className="flex flex-col justify-center items-center gap-12 m-16">
+      <h1 className="text-center">Trading Stocks</h1>
+
+      <p className="text-xl text-center">
+        You’ll be given a series of real-world news events from around the globe. You must decide whether to buy,
+        sell, or hold. Every decision affects your balance — some will pay off, others won’t. After all prompts, your
+        final balance determines your prize.
+      </p>
+
+      <div className="flex flex-col items-center gap-8 mt-8">
+        <Portfolio cash={cash} />
+        <TradePrompt prompt={prompt} />
+        <TradeActions
+          onBuy={handleBuy}
+          onSell={handleSell}
+          onHold={handleHold}
+        />
       </div>
+
+      <p className="text-xl text-center">
+        Think fast, trade smart, and maximise your gains 💸
+      </p>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
